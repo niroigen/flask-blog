518bbe06808205c90ec3f84a7bb82de456f52878
Niroigen Suntharam <niroigen@gmail.com>
2019-06-30 00:52:43 -0400

    Importing wraps from functool

--- src/shared/Authentication.py
+++ src/shared/Authentication.py
@@ -3,6 +3,7 @@ import os
 import datetime
 from flask import json
 from ..models.UserModel import UserModel
+from functools import wraps
 
 
 class Auth():
@@ -50,35 +51,35 @@ class Auth():
       return re
 
   @staticmethod
-    def auth_required(func):
-      """
-      Auth decorator
-      """
-      @wraps(func)
-      def decorated_auth(*args, **kwargs):
-        if 'api-token' not in request.headers:
-          return Response(
-            mimetype="application/json",
-            response=json.dumps({'error': 'Authentication token is not available, please login to get one'}),
-            status=400
-          )
-        token = request.headers.get('api-token')
-        data = Auth.decode_token(token)
-        if data['error']:
-          return Response(
-            mimetype="application/json",
-            response=json.dumps(data['error']),
-            status=400
-          )
-          
-        user_id = data['data']['user_id']
-        check_user = UserModel.get_one_user(user_id)
-        if not check_user:
-          return Response(
-            mimetype="application/json",
-            response=json.dumps({'error': 'user does not exist, invalid token'}),
-            status=400
-          )
-        g.user = {'id': user_id}
-        return func(*args, **kwargs)
-      return decorated_auth
+  def auth_required(func):
+    """
+    Auth decorator
+    """
+    @wraps(func)
+    def decorated_auth(*args, **kwargs):
+      if 'api-token' not in request.headers:
+        return Response(
+          mimetype="application/json",
+          response=json.dumps({'error': 'Authentication token is not available, please login to get one'}),
+          status=400
+        )
+      token = request.headers.get('api-token')
+      data = Auth.decode_token(token)
+      if data['error']:
+        return Response(
+          mimetype="application/json",
+          response=json.dumps(data['error']),
+          status=400
+        )
+        
+      user_id = data['data']['user_id']
+      check_user = UserModel.get_one_user(user_id)
+      if not check_user:
+        return Response(
+          mimetype="application/json",
+          response=json.dumps({'error': 'user does not exist, invalid token'}),
+          status=400
+        )
+      g.user = {'id': user_id}
+      return func(*args, **kwargs)
+    return decorated_auth
